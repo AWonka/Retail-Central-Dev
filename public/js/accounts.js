60077b834cc95ec8d1f8ac23d7507c758e13e97e
Austin Wonka <wonkaaustin@gmail.com>
2022-07-27 12:48:55 -0400

    changed customers to accounts and added most accounts pages

---
+++ public/js/accounts.js
@@ -0,0 +1,295 @@
+// CLUTTER INC
+if (document.title === "Clutter Inc") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/clutter/Clutter-head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Zach Nash',
+            'email': 'znash@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        },
+        {
+            'name': 'Jacksoun Routon',
+            'email': 'jrouton@prattindustries.com',
+            'lead': false,
+            'img': '/assets/images/signage/Angela Clark Fix.png',
+        }
+    ];
+};
+
+// DO IT BEST
+if (document.title === "Do It Best") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/doitbest/DoItBest-Head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Jacksoun Routon',
+            'email': 'jrouton@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        }
+    ];
+};
+
+// HOBBY LOBBY
+if (document.title === "Hobby Lobby") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/hobbylobby/hobby-lobby-head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Jace Denney',
+            'email': 'Jdenney@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        }
+    ];
+};
+
+// HOME DEPOT
+if (document.title === "Home Depot") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/home depot/THD-Head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Rick Philabaum',
+            'email': 'rphilabaum@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        },
+        {
+            'name': 'Brendan Rogers',
+            'email': 'brogers@prattindustries.com',
+            'lead': false,
+            'img': '/assets/images/signage/Angela Clark Fix.png',
+        },
+        {
+            'name': 'Zach Nash',
+            'email': 'znash@prattindustries.com',
+            'lead': false,
+            'img': '/assets/images/signage/Bernie Trueblood Fix.png',
+        }
+    ];
+};
+
+// HOME DEPOT CANADA
+if (document.title === "Home Depot Canada") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/home depot/THD-Head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Ryan Reynolds',
+            'email': 'rwreynolds@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        }
+    ];
+};
+
+// OFFICE DEPOT
+if (document.title === "Office Depot") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/officedepot/OD-Head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Jacksoun Routon',
+            'email': 'jrouton@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        },
+        {
+            'name': 'Jami Stanton',
+            'email': 'jstanton@prattindustries.com',
+            'lead': false,
+            'img': '/assets/images/signage/Angela Clark Fix.png',
+        }
+    ];
+};
+
+// PETCO
+if (document.title === "Petco") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/petco/Petco-Head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Scott Geltner',
+            'email': 'sgeltner@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        }
+    ];
+};
+
+// PUBLIC STORAGE
+if (document.title === "Public Storage") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/publicstorage/Public-Storage-head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Scott Mills',
+            'email': 'lsmills@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        }
+    ];
+};
+
+// USPS
+if (document.title === "USPS") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/usps/USPS-head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Jace Denney',
+            'email': 'Jdenney@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        },
+        {
+            'name': 'Jami Stanton',
+            'email': 'jstanton@prattindustries.com',
+            'lead': false,
+            'img': '/assets/images/signage/Angela Clark Fix.png',
+        }
+    ];
+};
+
+// WALMART
+if (document.title === "Walmart") {
+    var bannersData = [
+        {
+            'src': '/assets/images/accounts/walmart/Walmart-head.png',
+        }
+    ];
+
+    var employeeData = [
+        {
+            'name': 'Vandy Cameron',
+            'email': 'vjcameron2@prattindustries.com',
+            'lead': true,
+            'img': '/assets/images/signage/Rick Sensenbrenner Fix.png',
+        },
+        {
+            'name': 'Becca Iverson',
+            'email': 'riverson@prattindustries.com',
+            'lead': false,
+            'img': '/assets/images/signage/Angela Clark Fix.png',
+        },
+        {
+            'name': 'Shane Stewart',
+            'email': 'sstewart3@prattindustries.com',
+            'lead': false,
+            'img': '/assets/images/signage/Bernie Trueblood Fix.png',
+        },
+        {
+            'name': 'Jason Avila',
+            'email': 'javila@prattindustries.com',
+            'lead': false,
+            'img': '/assets/images/signage/Bernie Trueblood Fix.png',
+        },
+        {
+            'name': 'Kari Files',
+            'email': 'kfiles@prattindustries.com',
+            'lead': false,
+            'img': '/assets/images/signage/Bernie Trueblood Fix.png',
+        }
+    ];
+};
+
+let headerEl = document.querySelector('header');
+let leadEl = document.querySelector('.lead');
+let membersEl = document.querySelector('.team-members');
+
+console.log(document)
+
+// calling functions
+generateBanners();
+generateEmployees();
+
+
+// generate header and capabilities banners
+function generateBanners() {
+    const headerBanner = bannersData[0];
+    
+    const headerBannerEl = document.createElement('img');
+    headerBannerEl.setAttribute('src', headerBanner.src);
+    headerBannerEl.setAttribute('alt', 'signage/events banner');
+
+    headerEl.appendChild(headerBannerEl);
+};
+
+// generate team lead and members of team
+function generateEmployees() {
+    employeeData.forEach(employee => {
+
+        if(employee.lead === true) {
+
+            const teamLeadEl = document.createElement('div');
+            teamLeadEl.classList.add('team-lead');
+
+            teamLeadEl.innerHTML = `
+                <div class="lead-img">
+                    <img src="${employee.img}" alt="${employee.name} image">
+                </div>
+                <div class="lead-details">
+                    <h2>Key Contact:</h2>
+                    <p>${employee.name}</p>
+                    <p>Signage Lead</p>
+                    <a href="mailto:${employee.email}"><img title="${employee.email}" src="/assets/images/email-asset-128-fix.png" alt=""></a>
+                </div>
+            `
+
+            leadEl.appendChild(teamLeadEl);
+        };
+
+        if(employee.lead === false) {
+
+            const teamMembersEl = document.createElement('div');
+            teamMembersEl.classList.add('member');
+
+            teamMembersEl.innerHTML = `
+                <img class="member-img" src="${employee.img}" alt="${employee.name} image">
+                <p>${employee.name}</p>
+                <a href="mailto:${employee.email}"><img title="${employee.email}" src="/assets/images/email-asset-128-fix.png" alt=""></a>
+            `
+
+            membersEl.appendChild(teamMembersEl);
+        }
+    });
+};
